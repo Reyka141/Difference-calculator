389bb619be262350d8b1c6f332532c0f61f1214d
Andrii <youdronveduta@gmail.com>
2024-06-10 12:14:34 +0200

    update bin/gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,6 +3,9 @@ import { program } from 'commander';
 
 program
   .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0');
-  
+  .version('1.0.0')
+  .argument('<filepath1>')
+  .argument('<filepath2>',)
+  .option('-f, --format [type]', 'output format');
+
 program.parse();
