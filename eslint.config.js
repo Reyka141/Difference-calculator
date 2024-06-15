85c882f0e6a7d49483e49fc7e9eeb10e59b3951d
Andrii <youdronveduta@gmail.com>
2024-06-15 15:43:28 +0200

    update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,41 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 
 
+// export default [
+//   {languageOptions: { globals: globals.node }},
+//   pluginJs.configs.recommended,
+// ];
 export default [
-  {languageOptions: { globals: globals.node }},
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+      parserOptions: {
+        // Eslint doesn't supply ecmaVersion in `parser.js` `context.parserOptions`
+        // This is required to avoid ecmaVersion < 2015 error or 'import' / 'export' error
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    
+  },
   pluginJs.configs.recommended,
+  {
+    rules: {
+      'no-underscore-dangle': [
+        'error',
+        {
+          allow: ['__filename', '__dirname'],
+        },
+      ],
+      'import/no-named-as-default': 'off',
+      'import/no-named-as-default-member': 'off',
+      'no-console': 'off',
+      'import/no-extraneous-dependencies': 'off',
+    },
+    
+  },
+  
 ];
