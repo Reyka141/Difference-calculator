5a1bf6d77c3a14dc2de1515986f066add81655bc
Andrii <youdronveduta@gmail.com>
2024-06-24 19:02:44 +0200

    update src/gendiff-dif.js

--- src/gendiff-dif.js
+++ src/gendiff-dif.js
@@ -1,64 +1,45 @@
 import _ from 'lodash';
 import readingFile from './parsers.js';
+import stylish from '../src/stylish.js';
 
+const creatObjOfDiff = (file1, file2) => {
+  const keysFile1 = Object.keys(file1);
+  const keysFile2 = Object.keys(file2);
 
-const stringify = (value) => {
-    const iter = (currentValue, depth) => {
-      // альтернативный вариант: (typeof currentValue !== 'object' || currentValue === null)
-      if (!_.isObject(currentValue)) {
-        return `${currentValue}`;
-      }
-  
-      // const indentSize = depth * spacesCount;
-      // const currentIndent = replacer.repeat(indentSize);
-      // const bracketIndent = replacer.repeat(indentSize - spacesCount);
-      const lines = Object
-        .entries(currentValue)
-        .map(([key, val]) => `  ${key}: ${iter(val, depth + 1)}`);
-      return [
-        '{',
-        ...lines,
-        `}`,
-      ].join('\n');
-    };
-  
-    return iter(value, 1);
-  };
-  
+  const keys = _
+  .union(keysFile1, keysFile2)
+  .sort();
 
-
-const genDiff = (filepath1, filepath2) => {
-    const file1 = readingFile(filepath1);
-    const file2 = readingFile(filepath2);
-    if (file1 === undefined || file2 === undefined) {
-      return 'error of type file';
+  const objOfDiff = keys.reduce((acc, key) => {
+    if (!Object.hasOwn(file1, key)) {
+        acc = { ...acc, [`+ ${key}`]: file2[key]};
+    } else if (!Object.hasOwn(file2, key)) {
+        acc = { ...acc, [`- ${key}`]: file1[key]};
+    } else if (_.isObject(file1[key]) && _.isObject(file2[key])) {
+        acc = { ...acc, [`  ${key}`]: creatObjOfDiff(file1[key], file2[key])}; 
+    } else if (file1[key] !== file2[key]) {
+        acc = { ...acc, [`- ${key}`]: file1[key]};
+        acc = {... acc, [`+ ${key}`]: file2[key]};
+    } else {
+        acc = { ...acc, [`  ${key}`]: file1[key]};
     }
-    const keysFile1 = Object.keys(file1);
-    const keysFile2 = Object.keys(file2);
+   
+    return acc;
+  }, {});
 
-    const keys = _
-    .union(keysFile1, keysFile2)
-    .sort();
-
-    const objOfDiff = keys.reduce((acc, key) => {
-        if (!Object.hasOwn(file1, key)) {
-            acc = { ...acc, [`+ ${key}`]: file2[key]};
-        } else if (!Object.hasOwn(file2, key)) {
-            acc = { ...acc, [`- ${key}`]: file1[key]};
-        } else if (file1[key] !== file2[key]) {
-            acc = { ...acc, [`- ${key}`]: file1[key]};
-            acc = {... acc, [`+ ${key}`]: file2[key]};
-        } else {
-            acc = { ...acc, [`  ${key}`]: file1[key]};
-        }
-       
-        return acc;
-    }, {});
-    const stringOfObject = stringify(objOfDiff);
-
-
-    return stringOfObject;
+  return objOfDiff;
 }
 
-export { readingFile };
+const genDiff = (filepath1, filepath2) => {
+    const obj1 = readingFile(filepath1);
+    const obj2 = readingFile(filepath2);
+    if (obj1 === undefined || obj2 === undefined) {
+      return 'error of type file';
+    }
+    
+    const result = creatObjOfDiff(obj1, obj2);
+
+    return stylish(result);
+};
+
 export default genDiff;
