db0a59e201dd9a6e6ab7d936702c905a231fb101
Andrii <youdronveduta@gmail.com>
2024-06-24 19:02:33 +0200

    update bin/gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,14 +1,16 @@
 #!/usr/bin/env node
+
 import { program } from 'commander';
 import * as path from 'path';
 import genDiff from '../index.js';
+import stylish from '../src/stylish.js';
 
 program
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
   .argument('<filepath1>')
   .argument('<filepath2>',)
-  .option('-f, --format [type]', 'output format')
+  .option('-f, --format [type]', 'output format', stylish)
   .action((filepath1, filepath2) => {
     const result = genDiff(path.resolve(filepath1),path.resolve(filepath2));
     console.log(result);
