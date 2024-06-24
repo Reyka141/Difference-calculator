c497c4109f472721c1fac03a3d877cc36479e568
Andrii <youdronveduta@gmail.com>
2024-06-24 19:02:55 +0200

    add src/stylish.js

---
+++ src/stylish.js
@@ -0,0 +1,28 @@
+import _ from 'lodash';
+
+const stylish = (value) => {
+    const replacer = ' ';
+    const spacesCount = 4 ;
+    const iter = (currentValue, depth) => {
+      if (!_.isObject(currentValue)) {
+        return `${currentValue}`;
+      }
+  
+      const indentSize = depth * spacesCount ;
+      const bracketIndent = replacer.repeat(indentSize - spacesCount);
+     
+      const lines = Object
+        .entries(currentValue)
+        .map(([key, val]) => `${(key.includes(' ')) ? replacer.repeat(indentSize - 2) : replacer.repeat(indentSize)}${key}: ${iter(val, depth + 1)}`);
+  
+      return [
+        '{',
+        ...lines,
+        `${bracketIndent}}`,
+      ].join('\n');
+    };
+  
+    return iter(value, 1);
+  };
+
+  export default stylish;
