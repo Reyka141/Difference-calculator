1a0887605a36e21ab36f74ae58612200d0a1fae0
Andrii <youdronveduta@gmail.com>
2024-06-25 22:29:37 +0200

    update src/parsers.js

--- src/parsers.js
+++ src/parsers.js
@@ -3,12 +3,12 @@ import fs from 'node:fs';
 import path from 'path';
 
 const readingFile = (pathToFile) => {
-   
-    if (path.extname(pathToFile) === '.json') {
-        const file = fs.readFileSync(pathToFile, 'utf8');
+   const fullPath = path.resolve(pathToFile)
+    if (path.extname(fullPath) === '.json') {
+        const file = fs.readFileSync(fullPath, 'utf8');
         return JSON.parse(file);
-    } else if (path.extname(pathToFile) === '.yml' || path.extname(pathToFile) === '.yaml') {
-        const file = fs.readFileSync(pathToFile, 'utf8');
+    } else if (path.extname(fullPath) === '.yml' || path.extname(fullPath) === '.yaml') {
+        const file = fs.readFileSync(fullPath, 'utf8');
         return yaml.load(file);
     }
     return undefined;
