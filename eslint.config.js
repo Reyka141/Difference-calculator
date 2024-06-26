ab407e4383bec01c897b1df80254731f659ef88f
Andrii <youdronveduta@gmail.com>
2024-06-26 21:30:34 +0200

    update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -2,10 +2,7 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 
 
-// export default [
-//   {languageOptions: { globals: globals.node }},
-//   pluginJs.configs.recommended,
-// ];
+
 export default [
   {
     languageOptions: {
@@ -14,8 +11,6 @@ export default [
         ...globals.jest,
       },
       parserOptions: {
-        // Eslint doesn't supply ecmaVersion in `parser.js` `context.parserOptions`
-        // This is required to avoid ecmaVersion < 2015 error or 'import' / 'export' error
         ecmaVersion: 'latest',
         sourceType: 'module',
       },
