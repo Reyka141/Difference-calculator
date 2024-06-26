01b0952f3c81eeb82943d4440b0da5a3891b0bfe
Andrii <youdronveduta@gmail.com>
2024-06-26 21:58:42 +0200

    udpate bin/gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,12 +5,11 @@ import genDiff from '../index.js';
 program
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
-  .argument('<filepath1>')
-  .argument('<filepath2>',)
+  .argument('<filepath1>', '<filepath2>')
   .option('-f, --format [type]', 'output format', 'stylish')
   .action((filepath1, filepath2, options) => {
-    const result = genDiff(filepath1, filepath2, options.format);
-    console.log(result);
-  })
-  .parse();
+    console.log(genDiff(filepath1, filepath2, options.format));
+  });
+  
+program.parse();
 
