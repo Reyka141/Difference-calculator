81a684a95abb7243ff74d2fe91cb9b9b3d5d26b6
Andrii <youdronveduta@gmail.com>
2024-06-27 12:54:25 +0200

    update __test__/gendiff.test.js

--- __test__/gendiff.test.js
+++ __test__/gendiff.test.js
@@ -14,7 +14,7 @@ const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8'
 const pathToFile1 = getFixturePath('file1.json');
 const pathToFile2 = getFixturePath('file2.json');
 const pathToFile3 = getFixturePath('file1.yml');
-const pathToFile4 = getFixturePath('file2.yml');
+const pathToFile4 = getFixturePath('file2.yaml');
 const resultPlainFromat = readFile('result_plain.txt');
 const resultStylishFromat = readFile('result_stylish.txt');
 const resultJsonFormat = readFile('result_json.txt');
@@ -24,7 +24,7 @@ test('work with stylish format', () => {
 
   expect(genDiff(pathToFile3, pathToFile4)).toBe(resultStylishFromat);
 
-  expect(genDiff(pathToFile2, 'invalidformat')).toBe('error of type file');
+  expect(genDiff(pathToFile2, 'invalidformat.txt')).toBe('error of type file');
 });
 
 test('to work with plain format', () => {
@@ -32,11 +32,15 @@ test('to work with plain format', () => {
 
   expect(genDiff(pathToFile3, pathToFile4, 'plain')).toBe(resultPlainFromat);
 
-  expect(genDiff(pathToFile2, 'invalidformat', 'plain')).toBe('error of type file');
+  expect(genDiff(pathToFile2, 'invalidformat.txt', 'plain')).toBe('error of type file');
 });
 
 test('genDiff test json format', () => {
   expect(genDiff(pathToFile1, pathToFile2, 'json')).toEqual(resultJsonFormat);
 
-  expect(genDiff(pathToFile2, 'invalidformat', 'json')).toBe('error of type file');
+  expect(genDiff(pathToFile2, 'invalidformat.txt', 'json')).toBe('error of type file');
+});
+
+test('test with invalid format', () => {
+  expect(genDiff(pathToFile1, pathToFile2, 'yml')).toBe(undefined);
 });
