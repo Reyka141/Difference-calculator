09d0498a604c00eec0dceaf7b13f65ffa3fb1574
Andrii <youdronveduta@gmail.com>
2024-06-27 12:22:00 +0200

    update __test__/gendiff.test.js

--- __test__/gendiff.test.js
+++ __test__/gendiff.test.js
@@ -11,25 +11,13 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-let pathToFile1;
-let pathToFile2;
-let pathToFile3;
-let pathToFile4;
-let resultPlainFromat;
-let resultStylishFromat;
-beforeAll(() => {
-  pathToFile1 = getFixturePath('file1.json');
-  pathToFile2 = getFixturePath('file2.json');
-  pathToFile3 = getFixturePath('file1.yml');
-  pathToFile4 = getFixturePath('file2.yml');
-  resultPlainFromat = readFile('result_plain.txt');
-  resultStylishFromat = readFile('result_stylish.txt');
-});
-
-// test('test function readingFile', () => {
-//     console.log(getFixturePath('file1.json'));
-//     expect(readingFile(getFixturePath('file1.json'))).toEqual(JSON.parse(file1));
-// });
+const pathToFile1 = getFixturePath('file1.json');
+const pathToFile2 = getFixturePath('file2.json');
+const pathToFile3 = getFixturePath('file1.yml');
+const pathToFile4 = getFixturePath('file2.yml');
+const resultPlainFromat = readFile('result_plain.txt');
+const resultStylishFromat = readFile('result_stylish.txt');
+const resultJsonFormat = readFile('result_json.txt');
 
 test('test genDiff to work with stylish format', () => {
   expect(genDiff(pathToFile1, pathToFile2)).toBe(resultStylishFromat);
@@ -47,8 +35,8 @@ test('test genDiff to work with plain format', () => {
   expect(genDiff(pathToFile2, 'invalidformat', 'plain')).toBe('error of type file');
 });
 
-// test('test genDiff to work with json format', () => {
-//   expect(genDiff(pathToFile1, pathToFile2, 'json')).toEqual(resultJsonFormat);
+test('test genDiff to work with json format', () => {
+  expect(genDiff(pathToFile1, pathToFile2, 'json')).toEqual(resultJsonFormat);
 
-//   expect(genDiff(pathToFile2, 'invalidformat', 'json')).toBe('error of type file');
-// });
+  expect(genDiff(pathToFile2, 'invalidformat', 'json')).toBe('error of type file');
+});
